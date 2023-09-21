852d0b8eb9bc4b9b7c74d2ddd83bfcb199d0c329
AnthonyC27 <anthonyc1018@nycstudents.net>
2023-09-21 13:59:01 -0400

    Update project.py

--- project.py
+++ project.py
@@ -1,20 +1,17 @@
 
-x = "this is a thing"
-y= x.split( )
-z = y[0]
-print(y)
-print(z)
-
-x = "What if you do that"
-y= x.split( )
-z = y[0]
-print(y)
-print(z)
 
 
 
 
-noun = input("Noun: ")
-verb = input("Verb: ")
 
-madlibs =  "Hello! Welcome to my (noun).  "
+
+verb = input("Verb?")
+name = input("Name?")
+
+
+int(f" (Verb?) ")
+
+
+madlibs = f"I (verb?) to school, I wanted to pay attention to the class but (name?), the class clown, kept disturbing me. "
+
+print(madlibs)
