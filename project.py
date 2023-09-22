2a557a7fa32d1cc30b84f51c12cbfc92623ac66e
AnthonyC27 <anthonyc1018@nycstudents.net>
2023-09-21 22:47:30 -0400

    Update project.py

--- project.py
+++ project.py
@@ -1,3 +1,7 @@
+name = "Michael"
+verb = "ran"
+teacher = "Mr. Wilson"
+verb2 = "threw"
 
 
 
@@ -5,13 +9,9 @@
 
 
 
-verb = input("Verb?")
-name = input("Name?")
 
 
-int(f" (Verb?) ")
 
-
-madlibs = f"I (verb?) to school, I wanted to pay attention to the class but (name?), the class clown, kept disturbing me. "
+madlibs = f"I was about to be late to school, so I "+ verb +" to school, I wanted to pay attention to the class but "+ name +", the class clown, kept disturbing me. Then "+ teacher +" got involved and told "+ name +" to stop. During lunch I was eating and then "+ name +" "+ verb2 +" his food at me! I "+verb+" to him and "+verb2+" the rest of my food at him."
 
 print(madlibs)
