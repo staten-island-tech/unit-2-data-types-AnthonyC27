1fadf8e3d417e89dec34c6613ccfa3777d3c341a
AnthonyC27 <anthonyc1018@nycstudents.net>
2023-09-22 10:36:28 -0400

    Update project.py

--- project.py
+++ project.py
@@ -2,6 +2,7 @@ name = "Michael"
 verb = "ran"
 teacher = "Mr. Wilson"
 verb2 = "threw"
+noun = "birds"
 
 
 
@@ -12,6 +13,6 @@ verb2 = "threw"
 
 
 
-madlibs = f"I was about to be late to school, so I "+ verb +" to school, I wanted to pay attention to the class but "+ name +", the class clown, kept disturbing me. Then "+ teacher +" got involved and told "+ name +" to stop. During lunch I was eating and then "+ name +" "+ verb2 +" his food at me! I "+verb+" to him and "+verb2+" the rest of my food at him."
+madlibs = f"I was about to be late to school, so I "+ verb +" to school, I wanted to pay attention to the class but "+ name +", the class clown, kept disturbing me. Then "+ teacher +" got involved and told "+ name +" to stop. During lunch I was eating and then "+ name +" "+ verb2 +" his food at me! I "+verb+" to him and "+verb2+" the rest of my food at him. After School ended I went to the park with my friends, but "+noun+" kept bothering me, so I "+verb2+" a rock at them. "
 
 print(madlibs)
