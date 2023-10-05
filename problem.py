1517f3fbb5abe3a165bee5f3b1e4cedb8143348a
AnthonyC27 <anthonyc1018@nycstudents.net>
2023-10-05 13:44:11 -0400

    Update problem.py

--- problem.py
+++ problem.py
@@ -1,12 +1,14 @@
-x= bool(input("Do you have a soccer ball? True or False"))
-y = bool(input("Do you play soccer? True or False"))
+x= bool(input("Do you have a soccer ball? Yes or No, Do not type anything for No"))
+y = bool(input("Do you play soccer? Yes or No, Do not type anything for No"))
 output1 = ("You play soccer")
 output2 = ("You don't play soccer")
 output3 = ("You have a soccer ball")
 output4 = ("You are broke")
-if x == True and y == True:
+if (x,y)==(True,False):
     print(output1)
-elif  x == False and y == False:
+elif (x,y) == (False,False):
     print(output2)
-elif x == True and y == False:
+elif (x,y)==(True,False):
     print(output3)
+elif (x,y)==(False,True):
+    print(output4)
